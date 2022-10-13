9eb10ef8bc4d15ae3b6d4d93c66b9539375c8853
masiv <masiviady7@gmail.com>
2022-10-14 01:01:58 +0200

    ajout

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { GrCheckmark, GrEdit, GrTrash } from "react-icons/gr";
 
 const Todolist = ({ todos, setTodos }) => {
   return (
     <div>
       {todos.map((todo) => (
-        <li className="todo_list" key={todo.id}>
+        <li className="list" key={todo.id}>
           <input
             type="text"
             value={todo.title}
             className="result"
             onChange={(event) => event.preventDefault()}
           />
+
+          <div className="row">
+            <button className="button-complete task-button">
+              <GrCheckmark />
+            </button>
+            <button className="button-edit task-button">
+              <GrEdit />
+            </button>
+            <button className="button-delete task-button">
+              <GrTrash />
+            </button>
+          </div>
         </li>
       ))}
     </div>

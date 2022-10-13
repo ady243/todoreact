93ad1463298aba3ee4fcfcf267bc00e8beb67afb
masiv <masiviady7@gmail.com>
2022-10-13 17:31:18 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import Form from "./components/Form";
+import Todolist from "./components/Todolist";
 
 const App = () => {
   const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div class="container">
       <h1 className="titre">Todo List</h1>
-
-      <Form />
+      <Form
+        input={input}
+        setInput={setInput}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <div>
+        <Todolist todos={todos} setTodos={setTodos} />
+      </div>
+      <div className="footer"> By Ady</div>
     </div>
   );
 };
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,32 @@
-/* eslint-disable no-unreachable */
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function Form() {
+const Form = ({ input, setInput, todos, setTodos }) => {
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+    setInput("");
+  };
   return (
-    <form>
-      <input type="text" placeholder="Enter a todo ..." className="todo_in" />
-      <button className="=button-add" type="submit">
+    <form onSubmit={onFormSubmit}>
+      <input
+        type="text"
+        placeholder="Enter a todo ..."
+        className="todo_in"
+        value={input}
+        required
+        onChange={onInputChange}
+      />
+      <button className="button_add" type="submit">
         Add
       </button>
     </form>
   );
-}
+};
 
 export default Form;
